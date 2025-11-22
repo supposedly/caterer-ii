dd4bcd370746e48e49147855c8aea6cc35f5124f
speedydelete <speedykorok777@gmail.com>
2025-11-22 12:58:56 -0500

    stuff 2 and oops 25

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -17,18 +17,21 @@ client.on('messageCreate', async msg => {
     if (msg.author.bot || !sentByAdmin(msg) || !msg.content.startsWith('!!')) {
         return;
     }
-    let command: string;
-    if (msg.content === '!!start') {
-        command = 'systemctl start caterer';
-    } else if (msg.content === '!!stop') {
-        command = 'systemctl stop caterer';
-    } else if (msg.content === '!!restart') {
-        command = import.meta.dirname + '/../update.sh';
-    } else {
-        throw new Error('Invalid command!');
-    }
     try {
-        execSync(command);
+        if (msg.content === '!!start') {
+            execSync('systemctl start caterer');
+        } else if (msg.content === '!!stop') {
+            execSync('systemctl stop caterer');
+        } else if (msg.content === '!!restart') {
+            execSync('systemctl stop caterer');
+            execSync('systemctl start caterer');
+        } else if (msg.content === '!!update') {
+            execSync('systemctl stop caterer');
+            execSync(import.meta.dirname + '/../update.sh');
+            execSync('systemctl start caterer');
+        } else {
+            throw new Error('Invalid command!');
+        }
     } catch (error) {
         await msg.reply('`' + String(error) + '`');
         return;
