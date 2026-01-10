4bc46b5faeeb1dd55b04acf1fa909963f740bdc2
speedydelete <speedykorok777@gmail.com>
2026-01-10 14:30:28 -0500

    bruh why

--- src/wiki.ts
+++ src/wiki.ts
@@ -71,7 +71,7 @@ export async function cmdWiki(msg: Message, argv: string[]): Promise<Response> {
             if (typeof data === 'string') {
                 image = data;
             } else {
-                throw new Error(JSON.stringify(startData[Object.keys(startData)[0]], undefined, 4));
+                throw new Error(data + '\n\n' + JSON.stringify(startData[Object.keys(startData)[0]], undefined, 4));
             }
         } else {
             throw new Error(resp.status + ': ' + resp.statusText);
