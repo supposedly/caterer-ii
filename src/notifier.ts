c6be23c35a6442780a64c4aa28ef0c2cea266fa1
speedydelete <speedykorok777@gmail.com>
2026-01-19 11:27:50 -0500

    oooh

--- src/notifier.ts
+++ src/notifier.ts
@@ -3,61 +3,111 @@ import {TextChannel} from 'discord.js';
 import {TYPE_NAMES} from './db.js';
 
 
+function formatNewShips(category: 'speed' | 'period', type: string, data: [string, number][], bold: number): string {
+    let out: string[] = [];
+    for (let [speed, cells] of data) {
+        if (cells === bold) {
+            out.push(`**${speed} (${cells} cell${cells === 1 ? '' : 's'})**`);
+        } else {
+            out.push(`${speed} (${cells} cell${cells === 1 ? '' : 's'})`);
+        }
+    }
+    return `New ${category}${data.length === 1 ? '' : 's'} in ${type}: ${out.join(', ')}`;
+}
+
+function formatImprovedShips(category: 'speed' | 'period', type: string, data: [string, number, number][], bold: number): string {
+    let out: string[] = [];
+    for (let [speed, newCells, oldCells] of data) {
+        if (newCells === bold) {
+            out.push(`**${speed} (${oldCells} to ${newCells} cell${newCells === 1 ? '' : 's'})**`);
+        } else {
+            out.push(`${speed} (${oldCells} to ${newCells} cell${newCells === 1 ? '' : 's'})`);
+        }
+    }
+    return `Improved ${category}${data.length === 1 ? '' : 's'} in ${type}: ${out.join(', ')}`;
+}
+
+
+type ShipGroup = {newShips: [string, number][], newPeriods: [string, number][], improvedShips: [string, number, number][], improvedPeriods: [string, number, number][]};
+
 export async function check5S(channel: TextChannel): Promise<void> {
     let resp = await fetch('https://speedydelete.com/5s/api/getnewships');
-    if (resp.ok) {
-        let data = await resp.json() as {newShips: [string, string, number][], improvedShips: [string, string, number, number][]};
-        if (data.newShips.length === 0 && data.improvedShips.length === 0) {
-            return;
-        }
-        let lines: string[] = [];
-        for (let type of Object.keys(TYPE_NAMES)) {
-            let newShips = data.newShips.filter(x => x[0] === type);
-            let improvedShips = data.improvedShips.filter(x => x[0] === type);
-            if (newShips.length === 0 && improvedShips.length === 0) {
-                continue;
-            }
-            if (newShips.length > 0) {
-                if (newShips.length === 1) {
-                    lines.push(`New speed in ${TYPE_NAMES[type]}: ${newShips[0][2] === 3 ? `**${newShips[0][1]} (${newShips[0][2]} cells)**` : `${newShips[0][1]} (${newShips[0][2]} cells)`}`);
-                } else {
-                    lines.push(`${newShips.length} new speeds in ${TYPE_NAMES[type]}: ${newShips.map(x => x[2] === 3 ? `**${x[1]} (${x[2]} cells)**` : `${x[1]} (${x[2]} cells)`).join(', ')}`);
-                }
-            }
-            if (improvedShips.length > 0) {
-                if (improvedShips.length === 1) {
-                    lines.push(`Improved speed in ${TYPE_NAMES[type]}: ${improvedShips[0][2] === 3 ? `**${improvedShips[0][1]} (${improvedShips[0][3]} cells to ${improvedShips[0][2]} cells)**` : `${improvedShips[0][1]} (${improvedShips[0][3]} cells to ${improvedShips[0][2]} cells)`}`);
-                } else {
-                    lines.push(`${improvedShips.length} improved speeds in ${TYPE_NAMES[type]}: ${improvedShips.map(x => x[2] === 3 ? `**${x[1]} (${x[3]} cells to ${x[2]} cells)**` : `${x[1]} (${x[3]} cells to ${x[2]} cells)`).join(', ')}`);
-                }
-            }
-        }
-        let current = '';
-        for (let line of lines) {
-            let prev = current;
-            current += line + '\n';
-            if (current.length > 2000) {
-                if (prev !== '') {
-                    await channel.send(prev);
-                }
-                current = '';
-                while (line.length > 2000) {
-                    let index = line.slice(0, 1999).lastIndexOf(',');
-                    await channel.send(line.slice(0, index));
-                    line = line.slice(index);
-                }
-                await channel.send(line);
-            }
-        }
-        if (current !== '') {
-            while (current.length > 2000) {
-                let index = current.slice(0, 1999).lastIndexOf(',');
-                await channel.send(current.slice(0, index));
-                current = current.slice(index);
-            }
-            await channel.send(current);
-        }
-    } else {
+    if (!resp.ok) {
         console.log(`${resp.status} ${resp.statusText} while fetching new ships`);
+        return;
+    }
+    let data = await resp.json() as {newShips: [string, string, number][], improvedShips: [string, string, number, number][]};
+    if (data.newShips.length === 0 && data.improvedShips.length === 0) {
+        return;
+    }
+    let groups: {[key: string]: ShipGroup} = {};
+    for (let ship of data.newShips) {
+        let oscillator = ship[1].startsWith('p');
+        let data: ShipGroup;
+        if (ship[0] in groups) {
+            data = groups[ship[0]];
+        } else {
+            data = {newShips: [], newPeriods: [], improvedShips: [], improvedPeriods: []};
+        }
+        if (oscillator) {
+            data.newPeriods.push(ship.slice(1) as [string, number]);
+        } else {
+            data.newShips.push(ship.slice(1) as [string, number]);
+        }
+    }
+    for (let ship of data.newShips) {
+        let oscillator = ship[1].startsWith('p');
+        let data: ShipGroup;
+        if (ship[0] in groups) {
+            data = groups[ship[0]];
+        } else {
+            data = {newShips: [], newPeriods: [], improvedShips: [], improvedPeriods: []};
+        }
+        if (oscillator) {
+            data.improvedPeriods.push(ship.slice(1) as [string, number, number]);
+        } else {
+            data.improvedShips.push(ship.slice(1) as [string, number, number]);
+        }
+    }
+    let lines: string[] = [];
+    for (let key of Object.keys(groups).sort()) {
+        let data = groups[key];
+        if (data.newShips.length > 0) {
+            lines.push(formatNewShips('speed', key, data.newShips, 3));
+        }
+        if (data.newPeriods.length > 0) {
+            lines.push(formatNewShips('period', key, data.newPeriods, key.includes('b0') || key.includes('B0') ? 1 : 2));
+        }
+        if (data.improvedShips.length > 0) {
+            lines.push(formatImprovedShips('speed', key, data.improvedShips, 3));
+        }
+        if (data.improvedPeriods.length > 0) {
+            lines.push(formatImprovedShips('period', key, data.improvedPeriods, key.includes('b0') || key.includes('B0') ? 1 : 2));
+        }
+    }
+    let current = '';
+    for (let line of lines) {
+        let prev = current;
+        current += line + '\n';
+        if (current.length > 2000) {
+            if (prev !== '') {
+                await channel.send(prev);
+            }
+            current = '';
+            while (line.length > 2000) {
+                let index = line.slice(0, 1999).lastIndexOf(',');
+                await channel.send(line.slice(0, index));
+                line = line.slice(index);
+            }
+            await channel.send(line);
+        }
+    }
+    if (current !== '') {
+        while (current.length > 2000) {
+            let index = current.slice(0, 1999).lastIndexOf(',');
+            await channel.send(current.slice(0, index));
+            current = current.slice(index);
+        }
+        await channel.send(current);
     }
 }
