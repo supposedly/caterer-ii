5b1c23ec26ee57fc517ed5078ccbe0bf2df04903
speedydelete <speedykorok777@gmail.com>
2026-01-24 11:33:02 -0500

    YAY IT WORKS

--- src/help.ts
+++ src/help.ts
@@ -413,9 +413,9 @@ for (let cmd in HELP) {
     for (let arg of data.args) {
         msg += arg.newline ? '\n' : ' ';
         if (arg.optional) {
-            msg += '[' + arg.name + ']';
+            msg += '`[' + arg.name + ']`';
         } else {
-            msg += '<' + arg.name + '>';
+            msg += '`<' + arg.name + '>`';
         }
     }
     msg += '`\n\n' + data.desc + '\n\nArguments:';
