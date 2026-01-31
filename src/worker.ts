b3537cf7c70265d577f7605912aabc7e9e53cd77
speedydelete <speedykorok777@gmail.com>
2026-01-31 12:59:39 -0500

    ok...

--- src/worker.ts
+++ src/worker.ts
@@ -348,11 +348,10 @@ async function runSim(argv: string[], rle: string): Promise<[number, string | un
             gct[i++] = 0xff;
             gct[i++] = 0xff;
         } else if (p instanceof TreePattern && p.rule.colors && p.rule.colors[value]) {
-            throw new Error(`Loading colors for state ${value} (${p.states} states)`);
-            // let [r, g, b] = p.rule.colors[value];
-            // gct[i++] = r;
-            // gct[i++] = g;
-            // gct[i++] = b;
+            let [r, g, b] = p.rule.colors[value];
+            gct[i++] = r;
+            gct[i++] = g;
+            gct[i++] = b;
         } else if (p instanceof DataHistoryPattern || p instanceof CoordHistoryPattern) {
             let [r, g, b] = HISTORY_COLORS[value - 1];
             gct[i++] = r;
