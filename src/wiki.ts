9368bd962fcb1e2171b23cf3e980776b7bf766b7
speedydelete <speedykorok777@gmail.com>
2026-02-01 19:57:47 -0500

    noinclude

--- src/wiki.ts
+++ src/wiki.ts
@@ -118,7 +118,8 @@ export async function cmdWiki(msg: Message, argv: string[]): Promise<Response> {
             }
         }
     }
-    text = text.replaceAll(/<--.*?-->/g, '');
+    text = text.replaceAll(/<--(.*?)-->/g, '');
+    text = text.replaceAll(/<noinclude>(.*?)<\/noinclude>/g, '');
     text = text.replaceAll(/\{\{period\|(\d+)\}\}/g, '[period-$1](https://conwaylife.com/wiki/Category:Oscillators_with_period_$1)');
     text = text.replaceAll(/\{\{year\|(\d+)\}\}/g, '[$1](https://conwaylife.com/wiki/Category:Patterns_found_in_$1)');
     text = text.replaceAll(/<\/?references( \/)?>/g, '');
