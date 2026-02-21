f3a18ca9c61b3c0dca59a223e498f91a08356747
speedydelete <speedykorok777@gmail.com>
2026-02-21 12:27:55 -0500

    oops 495

--- src/notifier.ts
+++ src/notifier.ts
@@ -12,7 +12,7 @@ function formatNewShips(category: 'speed' | 'period', type: string, data: [strin
             out.push(`${speed} (${cells} cell${cells === 1 ? '' : 's'})`);
         }
     }
-    return `New ${category}${data.length === 1 ? '' : 's'} in ${type}: ${out.join(', ')}`;
+    return `${data.length} new ${category}${data.length === 1 ? '' : 's'} in ${type}: ${out.join(', ')}`;
 }
 
 function formatImprovedShips(category: 'speed' | 'period', type: string, data: [string, number, number][], bold: number): string {
@@ -24,7 +24,7 @@ function formatImprovedShips(category: 'speed' | 'period', type: string, data: [
             out.push(`${speed} (${oldCells} to ${newCells} cell${newCells === 1 ? '' : 's'})`);
         }
     }
-    return `Improved ${category}${data.length === 1 ? '' : 's'} in ${type}: ${out.join(', ')}`;
+    return `${data.length} improved ${category}${data.length === 1 ? '' : 's'} in ${type}: ${out.join(', ')}`;
 }
 
 
